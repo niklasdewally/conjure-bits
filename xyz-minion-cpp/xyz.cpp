3fb98e809e712301c13b2c9cf31e3cf82da6cf7b
Niklas Dewally <nd60@st-andrews.ac.uk>
2023-10-23 14:44:47 +0100

    WIP: segfaulting xyz

--- xyz-minion-cpp/xyz.cpp
+++ xyz-minion-cpp/xyz.cpp
@@ -22,29 +22,31 @@ int main(int argc, char *argv[])
 
   // encode the following model
   /*
-  MINION 3
-  **VARIABLES**
-  DISCRETE x #
-  {1..3}
-  DISCRETE y #
-  {1..3}
-  DISCRETE z #
-  {1..3}
-  **SEARCH**
-  PRINT[[x],[y],[z]]
-  VARORDER STATIC [x, y, z]
-  **CONSTRAINTS**
-  sumleq([x,y],z)
-  sumgeq([x,y],z)
-  **EOF**
-  */
+   * MINION 3
+   * **VARIABLES**
+   * DISCRETE x #
+   * {1..3}
+   * DISCRETE y #
+   * {2..4}
+   * DISCRETE z #
+   * {1..5}
+   * **SEARCH**
+   * PRINT[[x],[y],[z]]
+   * VARORDER STATIC [x, y, z]
+   * **CONSTRAINTS**
+   * sumleq([x,y,z],4)
+   * ineq(x, y, -1)
+   * **EOF**
+   */
 
-  std::vector<DomainInt> domain = {1,3};
+  std::vector<DomainInt> domainx = {1,3};
+  std::vector<DomainInt> domainy = {2,4};
+  std::vector<DomainInt> domainz = {1,5};
   
   // **VARIABLES**
-  newVar(instance,"x",VAR_DISCRETE,domain);
-  newVar(instance,"y",VAR_DISCRETE,domain);
-  newVar(instance,"z",VAR_DISCRETE,domain);
+  newVar(instance,"x",VAR_DISCRETE,domainx);
+  newVar(instance,"y",VAR_DISCRETE,domainy);
+  newVar(instance,"z",VAR_DISCRETE,domainz);
 
   Var x = instance.vars.getSymbol("x");
   Var y = instance.vars.getSymbol("y");
@@ -68,16 +70,18 @@ int main(int argc, char *argv[])
   
   ConstraintBlob leq(lib_getConstraint(ConstraintType::CT_LEQSUM));
   ConstraintBlob geq(lib_getConstraint(ConstraintType::CT_GEQSUM));
+  ConstraintBlob ineq(lib_getConstraint(ConstraintType::CT_INEQ));
 
   
   leq.vars.push_back({x,y});
-  leq.vars.push_back({z});
+  leq.constants.push_back({4});
                       
-  geq.vars.push_back({x,y});
-  geq.vars.push_back({z});
+  //ineq.vars.push_back({x});
+  //ineq.vars.push_back({y});
+  //ineq.constants.push_back({-1});
 
   instance.constraints.push_back(leq);
-  instance.constraints.push_back(geq);
+  //instance.constraints.push_back(ineq);
 
   for (int i=0;i<5;i++){
     printf("\n========== RUN %d ==========\n",i);

c76219b6ef371956eb6eb3988ef91bac37cade12
Niklas Dewally <niklas@dewally.com>
2025-04-08 15:27:16 +0100

    sort models so things run in a sensible order

--- 2025-tau-pythagorean-triples/bench.sh
+++ 2025-tau-pythagorean-triples/bench.sh
@@ -2,7 +2,7 @@
 
 mkdir -p data
 
-for model in $(find models_with_params/ -iname '*.eprime'); do 
-  hyperfine --runs 5 --warmup 1 --reference "savilerow -O0 -run-solver $model" "conjure_oxide solve -n 1 $model" --export-csv data/"$(basename $model)_results.csv"
+for model in $(find models_with_params/ -iname '*.eprime' | sort); do 
+  hyperfine --runs 3 --warmup 1 --reference "savilerow -O0 -run-solver $model" "ulimit -Sv 320000000 && exec conjure_oxide solve -n 1 $model" --export-csv data/"$(basename $model)_results.csv" 
 done
 

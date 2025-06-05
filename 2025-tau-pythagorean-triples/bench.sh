6ed55e90ae7b9e5e508f4ebbee3ae04eb1990ea6
Niklas Dewally <niklas@dewally.com>
2025-06-05 11:30:36 +0100

    modify tau for expand_ac

--- 2025-tau-pythagorean-triples/bench.sh
+++ 2025-tau-pythagorean-triples/bench.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+
+RUNS=1
 mkdir -p data
+MEM_ULIMIT_GB=16
 
 for model in $(find models_with_params/ -iname '*.eprime' | sort); do 
-  hyperfine --runs 3 --warmup 1 -n savilerow "savilerow -O0 -run-solver $model" -n conjureoxide "ulimit -Sv 320000000 && exec conjure_oxide solve -n 1 $model" --export-csv data/"$(basename $model)_results.csv" 
+  echo "--- $model ---"
+  hyperfine --runs ${RUNS} -n savilerow "ulimit -Sv ${MEM_ULIMIT_GB}000000 && exec savilerow -O0 -run-solver $model"\
+    -n conjureoxide_expand_simple "ulimit -Sv ${MEM_ULIMIT_GB}000000 && exec conjure_oxide --no-use-expand-ac solve -n 1 $model"\
+    -n conjureoxide_expand_ac     "ulimit -Sv ${MEM_ULIMIT_GB}000000 && exec conjure_oxide solve -n 1 $model"\
+    --export-csv data/"$(basename $model)_results.csv" 
 done
 
